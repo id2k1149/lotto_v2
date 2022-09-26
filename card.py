4b2b0934733cc857702489798fb7b23ad45812be
id2k1149 <id2k1149@gmail.com>
2022-09-26 10:57:51 -0400

    add card_is_empty

--- card.py
+++ card.py
@@ -9,6 +9,7 @@ class Card:
         cols = 9
         generator = NumbersGenerator(total_numbers, rows, cols)
         self.numbers = generator.random_numbers
+        self.numbers_after_cross_out = self.numbers.copy()
         self.numbers_on_card = generator.numbers_on_card
 
     def __str__(self):
@@ -29,6 +30,8 @@ class Card:
 
     def cross_out(self, item):
         if item in self.numbers:
+            index_to_delete = self.numbers_after_cross_out.index(item)
+            self.numbers_after_cross_out.pop(index_to_delete)
             number_index = self.numbers.index(item)
             cols_index = 8 if int(item) == 90 else int(item) // 10
             if number_index < 5:
@@ -41,12 +44,21 @@ class Card:
                 row = 2
                 self.numbers_on_card[row][cols_index] = '--'
 
+    def is_empty(self):
+        if len(self.numbers_after_cross_out) == 0:
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     card = Card()
     print(card)
     print(*card.numbers)
-    for each in card.numbers:
-        card.cross_out(each)
+    # number = card.numbers[0]
+    # card.cross_out(number)
+    # for each in card.numbers:
+    #     card.cross_out(each)
     print(card)
+    print(card.is_empty())
 
--- test_card.py
+++ test_card.py
@@ -7,3 +7,9 @@ class TestCard:
 
     def test_cross_out(self):
         self.card.cross_out(99)
+
+    def test_is_empty(self):
+        assert not self.card.is_empty()
+        for each in self.card.numbers:
+            self.card.cross_out(each)
+        assert self.card.is_empty()
